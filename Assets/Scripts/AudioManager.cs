4ac1ed83d641625546e5332749980bcd336d1ba2
Gugusea <augustin.guyot2@gmail.com>
2025-03-16 16:15:55 +0100

    VFX + SFX pre rebase
    Creation VFX vent + ajout sfx platform + gestion fadeIn/fadeOut + ajout gestion sfx restart

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -5,12 +6,12 @@ public class AudioManager : MonoBehaviour
     [Header("------- Audio Source -------")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] public AudioSource SFXSource;
-    [SerializeField] AudioSource AMBSourceWood1;
-    [SerializeField] AudioSource AMBSourceWood2;
-    [SerializeField] AudioSource AMBSourceGrass1;
-    [SerializeField] AudioSource AMBSourceGrass2;
-    [SerializeField] AudioSource AMBSourceRuche1;
-    [SerializeField] AudioSource AMBSourceRuche2;
+    [SerializeField] public AudioSource AMBSourceWood1;
+    [SerializeField] public AudioSource AMBSourceWood2;
+    [SerializeField] public AudioSource AMBSourceGrass1;
+    [SerializeField] public AudioSource AMBSourceGrass2;
+    [SerializeField] public AudioSource AMBSourceRuche1;
+    [SerializeField] public AudioSource AMBSourceRuche2;
 
     [Header("------- Music -------")]
     public AudioClip MUSBackground;
@@ -36,30 +37,27 @@ public class AudioManager : MonoBehaviour
 
     */
 
+    /*
+    [Header("SOUND GENERATOR")]
+
+    [SerializeField] private float minTimeBetweenSounds = 2.0f;
+    [SerializeField] private float maxTimeBetweenSounds = 10.0f;
+
+    [SerializeField] private float timeBetweenSound;
+    */
+
     [Header("------- Table of Audio -------")]
     public AudioClip[] tabAudioClick;
     public AudioClip[] tabAudioMvt;
 
     private int idLastAudioClick = -1;
     private int idLastAudioMvt = -1;
+    public float timeToFadeIn = 1f;
+    public float timeToFadeOut = 3f;
+    [SerializeField] private float volumeFadeInAmbiance = 0.8f;
 
     private void Start()
     {
-        StartAmbiance();
-    }
-
-    public void StartAmbiance()
-    {
-        //RESTART
-        musicSource.Stop();
-        SFXSource.Stop();
-        AMBSourceGrass1.Stop();
-        AMBSourceGrass2.Stop();
-        AMBSourceWood1.Stop();
-        AMBSourceWood2.Stop();
-        AMBSourceRuche1.Stop();
-        AMBSourceRuche2.Stop();
-
         musicSource.clip = MUSBackground;
         musicSource.Play();
 
@@ -73,7 +71,66 @@ public class AudioManager : MonoBehaviour
 
         AMBSourceRuche1.clip = AMBRuche1;
         AMBSourceRuche2.clip = AMBRuche2;
+        //StartAmbiance();
     }
+
+    public void StartAmbiance()
+    {
+        //RESTART
+        /*
+        SFXSource.Stop();
+        AMBSourceGrass1.Stop();
+        AMBSourceGrass2.Stop();
+        AMBSourceWood1.Stop();
+        AMBSourceWood2.Stop();
+        AMBSourceRuche1.Stop();
+        AMBSourceRuche2.Stop();
+        */
+        if (AMBSourceGrass1.isPlaying)
+        {
+            StartCoroutine(FadeAmbiance(false, AMBSourceGrass1, timeToFadeOut, 0f));
+        }
+        if (AMBSourceGrass2.isPlaying)
+        {
+            StartCoroutine(FadeAmbiance(false, AMBSourceGrass2, timeToFadeOut, 0f));
+        }
+        if (AMBSourceRuche1.isPlaying)
+        {
+            StartCoroutine(FadeAmbiance(false, AMBSourceRuche1, timeToFadeOut, 0f));
+        }
+        if (AMBSourceRuche2.isPlaying)
+        {
+            StartCoroutine(FadeAmbiance(false, AMBSourceRuche2, timeToFadeOut, 0f));
+        }
+
+        if(!AMBSourceWood1.isPlaying)
+        {
+            StartCoroutine(FadeAmbiance(true, AMBSourceWood1, timeToFadeIn, volumeFadeInAmbiance));
+        }
+        if (!AMBSourceWood2.isPlaying)
+        {
+            StartCoroutine(FadeAmbiance(true, AMBSourceWood2, timeToFadeIn, 0.5f));
+        }
+        //timeBetweenSound = GenerateRandomTimeBtwSound();
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        timeBetweenSound -= Time.deltaTime;
+        if (timeBetweenSound <= 0)
+        {
+            GenerateSound(1);
+            timeBetweenSound = GenerateRandomTimeBtwSound();
+        }
+    }
+
+    public float GenerateRandomTimeBtwSound()
+    {
+        return Random.Range(minTimeBetweenSounds, maxTimeBetweenSounds);
+    }
+    */
 
     public void GenerateSound(int typeOfSound) //1 = Ambiance , 2 = Click , 3 = Movement
     {
@@ -104,6 +161,26 @@ public class AudioManager : MonoBehaviour
         SFXSource.PlayOneShot(clip);
     }
 
+    public IEnumerator FadeAmbiance(bool fadeIn, AudioSource audioSource, float duration, float targetVolume)
+    {
+        if(!fadeIn)
+        {
+            double lengthOfSource = (double)audioSource.clip.samples / audioSource.clip.frequency;
+            yield return new WaitForSecondsRealtime((float)(lengthOfSource - duration));
+        }
+
+        float time = 0f;
+        float startVol = audioSource.volume;
+        while(time < duration)
+        {
+            time += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVol, targetVolume, time / duration);
+            yield return null;
+        }
+
+        yield break;
+    }
+
     public void ManageAmbiance(int requestId, int idAnswer) //idAnswer => 1 = A, 2 = B
     {
         switch (requestId)
@@ -113,20 +190,24 @@ public class AudioManager : MonoBehaviour
                 {
                     AMBSourceGrass1.Play();
                     AMBSourceGrass2.Play();
+                    StartCoroutine(FadeAmbiance(true, AMBSourceGrass1, timeToFadeIn, volumeFadeInAmbiance));
+                    StartCoroutine(FadeAmbiance(true, AMBSourceGrass2, timeToFadeIn, volumeFadeInAmbiance));
                 }
                 break;
 
             case 2:
                 if(idAnswer == 1)
                 {
+                    StartCoroutine(FadeAmbiance(false, AMBSourceWood1, timeToFadeOut, 0f));
                     AMBSourceWood1.Stop();
-                    AMBSourceWood2.Stop();
                 }
                 break;
 
             case 3:
                 if(idAnswer == 1)
                 {
+                    StartCoroutine(FadeAmbiance(false, AMBSourceGrass1, timeToFadeOut, 0f));
+                    StartCoroutine(FadeAmbiance(false, AMBSourceGrass2, timeToFadeOut, 0f));
                     AMBSourceGrass1.Stop();
                     AMBSourceGrass2.Stop();
                 }
@@ -137,6 +218,8 @@ public class AudioManager : MonoBehaviour
                 {
                     AMBSourceRuche1.Play();
                     AMBSourceRuche2.Play();
+                    StartCoroutine(FadeAmbiance(true, AMBSourceRuche1, timeToFadeIn, volumeFadeInAmbiance));
+                    StartCoroutine(FadeAmbiance(true, AMBSourceRuche2, timeToFadeIn, volumeFadeInAmbiance));
                 }
                 break;
 
