3cf646f0b953f6b58c7ea468fba28902f4878800
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-03-22 11:11:31 +0100

    adding cat placeholder and some corrections

--- Assets/Scripts/AchievementsManager.cs
+++ Assets/Scripts/AchievementsManager.cs
@@ -10,12 +10,16 @@ public class AchievementsManager : MonoBehaviour
     [SerializeField] private GameObject _birdWestGO;
     [SerializeField] private GameObject corneilleUIInfo;
     [SerializeField] private Sprite corneilleSprite;
+    [SerializeField] private GameObject inGameIconCorneille;
     private bool corneilleUnlock;
 
     private bool checkCorneille()
     {
         if(_birdWestGO.activeSelf)
+        {
             corneilleUnlock = true;
+            inGameIconCorneille.SetActive(true);
+        }
         return corneilleUnlock;
     }
 
@@ -34,12 +38,16 @@ public class AchievementsManager : MonoBehaviour
     [SerializeField] private GameObject _fliesVFXGO;
     [SerializeField] private GameObject insectesUIInfo;
     [SerializeField] private Sprite insectesSprite;
+    [SerializeField] private GameObject inGameIconInsectes;
     private bool insectesUnlock;
 
     private bool checkInsectes()
     {
         if (_fliesVFXGO.activeSelf)
+        {
             insectesUnlock = true;
+            inGameIconInsectes.SetActive(true);
+        }
         return insectesUnlock;
     }
 
@@ -53,17 +61,49 @@ public class AchievementsManager : MonoBehaviour
     }
     #endregion
 
+    #region chat
+    [Header("Chat")]
+    [SerializeField] private GameObject chatGO;
+    [SerializeField] private GameObject chatUIInfo;
+    [SerializeField] private Sprite chatSprite;
+    [SerializeField] private GameObject inGameIconChat;
+    private bool chatUnlock;
+
+    private bool checkChat()
+    {
+        if (chatGO.activeSelf)
+        {
+            chatUnlock = true;
+            inGameIconChat.SetActive(true);
+        }
+        return chatUnlock;
+    }
+
+    public void ShowChatInfo()
+    {
+        HideAllInfo();
+        if (chatUnlock)
+            chatUIInfo.SetActive(true);
+        else
+            ShowNotObtain();
+    }
+    #endregion
+
     #region FleursA
     [Header("FleursA")]
     [SerializeField] private GameObject _FleursAGO;
     [SerializeField] private GameObject FleursAUIInfo;
     [SerializeField] private Sprite fleursASprite;
+    [SerializeField] private GameObject inGameIconFleursA;
     private bool FleursAUnlock;
 
     private bool checkFleursA()
     {
         if (_FleursAGO.activeSelf)
+        {
             FleursAUnlock = true;
+            inGameIconFleursA.SetActive(true);
+        }
         return FleursAUnlock;
     }
 
@@ -82,12 +122,16 @@ public class AchievementsManager : MonoBehaviour
     [SerializeField] private GameObject _FleursBGO;
     [SerializeField] private GameObject FleursBUIInfo;
     [SerializeField] private Sprite fleursBSprite;
+    [SerializeField] private GameObject inGameIconFleursB;
     private bool FleursBUnlock;
 
     private bool checkFleursB()
     {
         if (_FleursBGO.activeSelf)
+        {
             FleursBUnlock = true;
+            inGameIconFleursB.SetActive(true);
+        }
         return FleursBUnlock;
     }
 
@@ -105,6 +149,7 @@ public class AchievementsManager : MonoBehaviour
     [Header("Archi")]
     [SerializeField] private GameObject ArchiUIInfo;
     [SerializeField] private Sprite archiSprite;
+    [SerializeField] private GameObject inGameIconArchi;
     private bool ArchiUnlock;
 
     public IEnumerator setArchiUnlock()
@@ -123,6 +168,12 @@ public class AchievementsManager : MonoBehaviour
         }
     }
 
+    public void setArchiUnlockIcon()
+    {
+        ArchiUnlock = true;
+        inGameIconArchi.SetActive(true);
+    }
+
     public void ShowArchiInfo()
     {
         HideAllInfo();
@@ -137,6 +188,7 @@ public class AchievementsManager : MonoBehaviour
     [Header("paysager")]
     [SerializeField] private GameObject paysagerUIInfo;
     [SerializeField] private Sprite paysagerSprite;
+    [SerializeField] private GameObject inGameIconPaysager;
     private bool paysagerUnlock;
 
     public IEnumerator setPaysagerUnlock()
@@ -155,6 +207,12 @@ public class AchievementsManager : MonoBehaviour
         }
     }
 
+    public void setPaysagerUnlockIcon()
+    {
+        paysagerUnlock = true;
+        inGameIconPaysager.SetActive(true);
+    }
+
     public void ShowPaysagerInfo()
     {
         HideAllInfo();
@@ -169,6 +227,7 @@ public class AchievementsManager : MonoBehaviour
     [Header("ecolo")]
     [SerializeField] private GameObject ecoloUIInfo;
     [SerializeField] private Sprite ecoloSprite;
+    [SerializeField] private GameObject inGameIconEcolo;
     private bool ecoloUnlock;
 
     public IEnumerator setEcoloUnlock()
@@ -187,6 +246,12 @@ public class AchievementsManager : MonoBehaviour
         }
     }
 
+    public void setEcoloUnlockIcon()
+    {
+        ecoloUnlock = true;
+        inGameIconEcolo.SetActive(true);
+    }
+
     public void ShowEcoloInfo()
     {
         HideAllInfo();
@@ -201,6 +266,7 @@ public class AchievementsManager : MonoBehaviour
     [Header("hybrid")]
     [SerializeField] private GameObject hybridUIInfo;
     [SerializeField] private Sprite hybridSprite;
+    [SerializeField] private GameObject inGameIconHybrid;
     private bool hybridUnlock;
 
     public IEnumerator setHybridUnlock()
@@ -217,6 +283,12 @@ public class AchievementsManager : MonoBehaviour
             achievementAnimator.SetTrigger("Popup");
             animationPopupAvailable = false;
         }
+    }
+
+    public void setHybridUnlockIcon()
+    {
+        hybridUnlock = true;
+        inGameIconHybrid.SetActive(true);
     }
 
     public void ShowHybridInfo()
@@ -272,6 +344,15 @@ public class AchievementsManager : MonoBehaviour
         }
     }
 
+    public void CheckAllAchievementsIcon()
+    {
+        checkCorneille();
+        checkInsectes();
+        checkChat();
+        checkFleursA();
+        checkFleursB();
+    }
+
     #region UI
     [Header("General UI")]
     [SerializeField] private GameObject notObtainUI;
@@ -284,12 +365,26 @@ public class AchievementsManager : MonoBehaviour
         notObtainUI.SetActive(false);
         corneilleUIInfo.SetActive(false);
         insectesUIInfo.SetActive(false);
+        chatUIInfo.SetActive(false);
         FleursAUIInfo.SetActive(false);
         FleursBUIInfo.SetActive(false);
         ArchiUIInfo.SetActive(false);
         paysagerUIInfo.SetActive(false);
         ecoloUIInfo.SetActive(false);
         hybridUIInfo.SetActive(false);
+    }
+
+    public void HideAllInGameIcon()
+    {
+        inGameIconCorneille.SetActive(false);
+        inGameIconInsectes.SetActive(false);
+        inGameIconChat.SetActive(false);
+        inGameIconFleursA.SetActive(false);
+        inGameIconFleursB.SetActive(false);
+        inGameIconHybrid.SetActive(false);
+        inGameIconPaysager.SetActive(false);
+        inGameIconEcolo.SetActive(false);
+        inGameIconArchi.SetActive(false);
     }
 
     private void ShowNotObtain()
