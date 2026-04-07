9c01fcdedb0cb03ca8f027ef0e6c682b636a8fd8
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2026-04-07 10:51:51 +0200

    Upgrade to unity 6.3.12

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,9 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using DefaultNamespace;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
 public class GameManager : MonoBehaviour
 {
     [Header("Cemetery")]
